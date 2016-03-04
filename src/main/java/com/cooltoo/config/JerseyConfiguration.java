ea8b83d295ae26c24c740424634dc8f547f2a1c1
ZhaoYi <yzzhao@thoughtworks.com>
2016-03-04 09:35:47 +0800

    fix merge issue

--- src/main/java/com/cooltoo/config/JerseyConfiguration.java
+++ src/main/java/com/cooltoo/config/JerseyConfiguration.java
@@ -3,12 +3,9 @@ package com.cooltoo.config;
 import com.cooltoo.api.BadgeAPI;
 import com.cooltoo.api.NurseAPI;
 import com.cooltoo.api.OrderAPI;
-<<<<<<< 747cce7cd3a899adcbb29d718ca30ec0da0d034a
 import com.cooltoo.api.PatientAPI;
-=======
 import com.cooltoo.filter.LoginAuthentication;
 import com.cooltoo.filter.NurseLoginAuthenticationFilter;
->>>>>>> add access token
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
