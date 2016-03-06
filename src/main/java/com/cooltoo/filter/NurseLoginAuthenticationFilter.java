b4ce8cf3405748a66d299b777969b258f08892b2
ZhaoYi <yzzhao@thoughtworks.com>
2016-03-06 19:52:13 +0800

    update login token check

--- src/main/java/com/cooltoo/filter/NurseLoginAuthenticationFilter.java
+++ src/main/java/com/cooltoo/filter/NurseLoginAuthenticationFilter.java
@@ -1,16 +1,22 @@
 package com.cooltoo.filter;
 
 import com.cooltoo.constants.ContextKeys;
+import com.cooltoo.constants.HeaderKeys;
+import com.cooltoo.entities.TokenAccessEntity;
 import com.cooltoo.exception.BadRequestException;
 import com.cooltoo.exception.ErrorCode;
+import com.cooltoo.repository.TokenAccessRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -27,6 +33,9 @@ public class NurseLoginAuthenticationFilter implements ContainerRequestFilter {
     @Context
     private ResourceInfo resourceInfo;
 
+    @Autowired
+    private TokenAccessRepository tokenAccessRepository;
+
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         LoginAuthentication login = resourceInfo.getResourceClass().getAnnotation(LoginAuthentication.class);
@@ -35,11 +44,28 @@ public class NurseLoginAuthenticationFilter implements ContainerRequestFilter {
             return;
         }
         if (login.requireNurseLogin()) {
-            Object nurse = servletRequest.getSession().getAttribute(ContextKeys.NURSE_LOGIN);
-            if (nurse == null) {
+
+
+            MultivaluedMap<String, String> pathParameters = requestContext.getHeaders();
+            List<String> tokens = pathParameters.get(HeaderKeys.ACCESS_TOKEN);
+            if (tokens == null || tokens.isEmpty()) {
                 throw new BadRequestException(ErrorCode.NOT_LOGIN);
             }
-            logger.info("nurse login as " + nurse);
+            if (tokens == null || tokens.isEmpty()) {
+                return;
+            }
+            String token = tokens.get(0);
+            logger.info("get token " + token);
+            requestContext.setProperty(HeaderKeys.ACCESS_TOKEN, token);
+            if (token != null) {
+                //read user id from token
+                List<TokenAccessEntity> tokenEntities = tokenAccessRepository.findTokenAccessByToken(token);
+                if (tokenEntities.isEmpty()) {
+                    throw new BadRequestException(ErrorCode.NOT_LOGIN);
+                }
+                requestContext.setProperty(ContextKeys.NURSE_LOGIN, tokenEntities.get(0).getId());
+            }
+
         }
     }
 }
--- src/main/java/com/cooltoo/filter/TokenAccess.java
+++
@@ -1,9 +0,0 @@
-package com.cooltoo.filter;
-
-/**
- * Created by yzzhao on 3/6/16.
- */
-public @interface TokenAccess {
-
-    boolean requireAccessToken() default false;
-}
