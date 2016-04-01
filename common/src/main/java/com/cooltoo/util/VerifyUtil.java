9c7271adbb7c97fcae8f94db0e22ce55926bf44d
ZhaoYi <yzzhao@thoughtworks.com>
2016-04-01 20:26:39 +0800

    add comment on verify ids

--- common/src/main/java/com/cooltoo/util/VerifyUtil.java
+++ common/src/main/java/com/cooltoo/util/VerifyUtil.java
@@ -1,5 +1,8 @@
 package com.cooltoo.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by zhaolisong on 16/3/22.
  */
@@ -8,6 +11,7 @@ public class VerifyUtil {
     public static final String ADMIN_USER_PASSWORD_REGEXP = "[a-z|A-Z|0-9]{32,128}";
     public static final String EMAIL_REGEXP = "^[a-zA-Z][a-zA-Z0-9_\\-]+[@][a-zA-Z_\\-\\.]+[\\.][a-zA-Z]+$";
     public static final String OCCUPATION_SKILL_ID = "[[0-9]+,]*[0-9]+";
+    private static final Logger logger = LoggerFactory.getLogger(VerifyUtil.class);
 
     public static boolean isStringEmpty(String string) {
         if (string instanceof String) {
@@ -31,6 +35,7 @@ public class VerifyUtil {
     }
 
     public static boolean isOccupationSkillIds(String ids) {
+        logger.info("get occupation skill ids "+ids);
         if (ids instanceof String) {
             return ids.matches(OCCUPATION_SKILL_ID);
         }
