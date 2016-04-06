d0954a9bfc6201932adb743132251b4bbda5bfc0
lisongzhao0 <lisongzhao0@msn.cn>
2016-04-06 10:35:30 +0800

    add getting friends all skills API

--- app_backend/src/main/java/com/cooltoo/backend/api/NurseOccupationSkillAPI.java
+++ app_backend/src/main/java/com/cooltoo/backend/api/NurseOccupationSkillAPI.java
@@ -4,6 +4,8 @@ import com.cooltoo.backend.beans.NurseOccupationSkillBean;
 import com.cooltoo.backend.filter.LoginAuthentication;
 import com.cooltoo.backend.services.NurseOccupationSkillService;
 import com.cooltoo.constants.ContextKeys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,16 +21,17 @@ import java.util.List;
 @Path("/nurse/skill")
 public class NurseOccupationSkillAPI {
 
+    private static final Logger logger = LoggerFactory.getLogger(NurseOccupationSkillAPI.class.getName());
+
     @Autowired
     private NurseOccupationSkillService skillService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @LoginAuthentication(requireNurseLogin = true)
-    public Response getAllSkills(
-            @Context HttpServletRequest request
-    ) {
+    public Response getAllSkills(@Context HttpServletRequest request) {
         long userId = (Long)request.getAttribute(ContextKeys.NURSE_LOGIN_USER_ID);
+        logger.info("User {} get all skills.", userId);
         List<NurseOccupationSkillBean> skills = skillService.getAllSkills(userId);
         return Response.ok(skills).build();
     }
@@ -37,23 +40,36 @@ public class NurseOccupationSkillAPI {
     @Path("/{skill_id}")
     @Produces(MediaType.APPLICATION_JSON)
     @LoginAuthentication(requireNurseLogin = true)
-    public Response getSkills(
-            @Context HttpServletRequest request,
-            @PathParam("skill_id") int skillId
+    public Response getSkills(@Context HttpServletRequest request,
+                              @PathParam("skill_id") int skillId
     ) {
         long userId = (Long)request.getAttribute(ContextKeys.NURSE_LOGIN_USER_ID);
+        logger.info("User {} get skill {}.", userId, skillId);
         NurseOccupationSkillBean skill = skillService.getSkill(userId, skillId);
         return Response.ok(skill).build();
+    }
+
+    @GET
+    @Path("/friend/{friend_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @LoginAuthentication(requireNurseLogin = true)
+    public Response getFriendSkill(@Context HttpServletRequest request,
+                                   @PathParam("friend_id") long friendId
+    ) {
+        long userId = (Long)request.getAttribute(ContextKeys.NURSE_LOGIN_USER_ID);
+        logger.info("User {} get friend {} 's skills.", userId, friendId);
+        List<NurseOccupationSkillBean> skills = skillService.getAllSkills(friendId);
+        return Response.ok(skills).build();
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @LoginAuthentication(requireNurseLogin = true)
-    public Response addSkill(
-            @Context HttpServletRequest request,
-            @FormParam("skill_id") int skillId
+    public Response addSkill(@Context HttpServletRequest request,
+                             @FormParam("skill_id") int skillId
     ) {
         long userId = (Long)request.getAttribute(ContextKeys.NURSE_LOGIN_USER_ID);
+        logger.info("User {} add skill {}.", userId, skillId);
         skillService.addSkill(userId, skillId);
         return Response.ok(skillId).build();
     }
@@ -62,11 +78,11 @@ public class NurseOccupationSkillAPI {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @LoginAuthentication(requireNurseLogin = true)
-    public Response addSkills(
-            @Context HttpServletRequest request,
-            @FormParam("skill_ids") String skillIds
+    public Response addSkills(@Context HttpServletRequest request,
+                              @FormParam("skill_ids") String skillIds
     ) {
         long userId = (Long)request.getAttribute(ContextKeys.NURSE_LOGIN_USER_ID);
+        logger.info("User {} add skills {} with batch adding.", userId, skillIds);
         skillService.addSkills(userId, skillIds);
         return Response.ok(skillIds).build();
     }
