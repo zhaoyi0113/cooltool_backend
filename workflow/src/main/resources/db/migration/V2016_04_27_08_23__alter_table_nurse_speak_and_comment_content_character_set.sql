66b3c76a5ddd8429a8446665cd1ec1acacc3f0a0
lisongzhao0 <lisongzhao0@msn.cn>
2016-04-27 20:34:03 +0800

    modify the column of nurse speak and comment character set to utf8mb4

---
+++ workflow/src/main/resources/db/migration/V2016_04_27_08_23__alter_table_nurse_speak_and_comment_content_character_set.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `nurse_speak`         CHANGE COLUMN `content` `content` VARCHAR(512) CHARACTER SET 'utf8mb4' NULL DEFAULT NULL ;
+ALTER TABLE `nurse_speak_comment` CHANGE COLUMN `comment` `comment` VARCHAR(512) CHARACTER SET 'utf8mb4' NULL DEFAULT NULL ;
