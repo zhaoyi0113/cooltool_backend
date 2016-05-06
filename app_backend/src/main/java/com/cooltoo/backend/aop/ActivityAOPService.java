f6f2a100d64a614d439ecb593e0b00d08a217c5f
ZhaoYi <yzzhao@thoughtworks.com>
2016-05-06 11:24:51 +0800

    add activity notification log

--- app_backend/src/main/java/com/cooltoo/backend/aop/ActivityAOPService.java
+++ app_backend/src/main/java/com/cooltoo/backend/aop/ActivityAOPService.java
@@ -7,6 +7,8 @@ import com.cooltoo.constants.ActivityStatus;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,29 +21,30 @@ import java.util.HashMap;
 @Component
 public class ActivityAOPService {
 
+    private static final Logger logger = LoggerFactory.getLogger(ActivityAOPService.class);
 
     @Autowired
     private NotificationCenter notificationCenter;
 
     @AfterReturning(pointcut = "execution(* com.cooltoo.services.ActivityService.createActivity(..))",
             returning = "retVal")
-    public void afterCreateActivity(JoinPoint joinPoint, ActivityBean retVal){
+    public void afterCreateActivity(JoinPoint joinPoint, ActivityBean retVal) {
         publishActivityNotification(retVal);
     }
 
     @AfterReturning(pointcut = "execution(* com.cooltoo.services.ActivityService.updateActivityStatus(..))",
             returning = "retVal")
-    public void afterUpdateActivity(JoinPoint joinPoint, ActivityBean retVal){
+    public void afterUpdateActivity(JoinPoint joinPoint, ActivityBean retVal) {
         publishActivityNotification(retVal);
     }
 
     private void publishActivityNotification(ActivityBean retVal) {
         if (retVal != null && ActivityStatus.ENABLE.equals(retVal.getStatus())) {
-            String bodyText = "官方发布新活动 "+retVal.getTitle();
+            logger.info("publish official activity notification");
+            String bodyText = "官方发布新活动 " + retVal.getTitle();
             notificationCenter.publishToAllDevices(bodyText, new HashMap<String, String>(), NotificationCode.PUBLISH_ACTIVITY);
         }
     }
-
 
 
 }
