eabe6fd183eb0d38b7d62d4797a641bea061db02
ZhaoYi <yzzhao@thoughtworks.com>
2016-05-20 20:47:20 +0800

    update apns action code

--- app_backend/src/main/java/com/cooltoo/backend/services/notification/NotificationCenter.java
+++ app_backend/src/main/java/com/cooltoo/backend/services/notification/NotificationCenter.java
@@ -10,8 +10,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by yzzhao on 4/29/16.
@@ -24,6 +27,10 @@ public class NotificationCenter {
     private static final String P12_PASSWORD = "!Yqt0529*";
 
     private static final Logger logger = LoggerFactory.getLogger(NotificationCenter.class);
+
+    private static final String NOTIFICATION_TYPE = "NOTIFICATION_TYPE";
+
+    private ExecutorService executorService = Executors.newCachedThreadPool();
 
     @Autowired
     private NurseDeviceTokensService deviceTokensService;
@@ -51,10 +58,16 @@ public class NotificationCenter {
         publishToDevice(apnsService, payload, tokens);
     }
 
-    private void publishToDevice(ApnsService apnsService, String payload, List<NurseDeviceTokensBean> deviceTokens) {
-        for(NurseDeviceTokensBean token : deviceTokens){
-            logger.info("push to device "+payload+","+token.getDeviceToken());
-            apnsService.push(token.getDeviceToken(), payload);
+    private void publishToDevice(final ApnsService apnsService, final String payload, List<NurseDeviceTokensBean> deviceTokens) {
+        for(final NurseDeviceTokensBean token : deviceTokens){
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    logger.debug("push to device "+payload+","+token.getDeviceToken());
+                    apnsService.push(token.getDeviceToken(), payload);
+                }
+            });
+
         }
     }
 
@@ -65,16 +78,15 @@ public class NotificationCenter {
     }
 
     private String createPayload(String bodyText, Map<String, String> customFields, String action) {
-        ApnsService apnsService = APNS.newService().withCert(getClass().
-                getResourceAsStream(P12_CER_FILE), P12_PASSWORD).
-                withSandboxDestination().build();
-        PayloadBuilder badge = APNS.newPayload().alertBody(bodyText).sound("default");
-        if(customFields != null) {
+        PayloadBuilder payloadBuilder = APNS.newPayload().alertBody(bodyText).sound("default").customField(NOTIFICATION_TYPE, action);
+//        PayloadBuilder payloadBuilder = APNS.newPayload().instantDeliveryOrSilentNotification().customField(NOTIFICATION_TYPE, action);
+        if(customFields != null){
             for (Map.Entry<String, String> entry : customFields.entrySet()) {
-                badge = badge.customField(entry.getKey(), entry.getValue());
+                payloadBuilder = payloadBuilder.customField(entry.getKey(), entry.getValue());
             }
         }
-        return badge.actionKey(action).build();
+
+        return payloadBuilder.build();
     }
 
 }
