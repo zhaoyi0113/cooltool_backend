acba53f8ac611776bc741b9e494883cd70dd6d25
lisongzhao0 <lisongzhao0@msn.cn>
2016-05-20 10:17:10 +0800

    activity test

--- workflow/src/test/java/com/cooltoo/services/ActivityServiceTest.java
+++ workflow/src/test/java/com/cooltoo/services/ActivityServiceTest.java
@@ -15,8 +15,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.math.BigDecimal;
