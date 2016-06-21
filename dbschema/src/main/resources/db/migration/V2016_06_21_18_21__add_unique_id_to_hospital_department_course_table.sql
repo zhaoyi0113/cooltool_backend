7cc97f5ef0f74bc12fecda526511c11957280d53
lisongzhao0 <lisongzhao0@msn.cn>
2016-06-21 18:33:53 +0800

    add unique id to hospital department course table for generate 2D bar code

---
+++ dbschema/src/main/resources/db/migration/V2016_06_21_18_21__add_unique_id_to_hospital_department_course_table.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `cooltoo_hospital` ADD COLUMN `unique_id` VARCHAR(100) NULL DEFAULT '';
+ALTER TABLE `go2nurse_course` ADD COLUMN `unique_id` VARCHAR(100) NULL DEFAULT '';
+ALTER TABLE `cooltoo_hospital_department` ADD COLUMN `unique_id` VARCHAR(100) NULL DEFAULT '';
