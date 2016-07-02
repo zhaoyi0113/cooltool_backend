378fa8fef246080904e0a3999c8519d2601b837c
lisongzhao0 <lisongzhao0@msn.cn>
2016-07-02 14:03:45 +0800

    add patient id to user questionnaire answer table, and add sequence to question table

---
+++ dbschema/src/main/resources/db/migration/V2016_07_02_13_51__add_patient_id_to_user_questionnaire_answer_table_and_add_squence_to_question_table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `go2nurse_user_questionnaire_answer` ADD COLUMN `patient_id` BIGINT(64) NULL DEFAULT 0 AFTER `user_id`;
+ALTER TABLE `go2nurse_question` ADD COLUMN `sequence` INT(11) NULL DEFAULT 0 AFTER `type`;
