de571f4618a3b70aff8138e2cca794f5cf1f81f1
lisongzhao0 <lisongzhao0@msn.cn>
2016-07-17 17:32:10 +0800

    change service category item order tables id to autogenerated

---
+++ dbschema/src/main/resources/db/migration/V2016_07_17_17_19__update_service_category_item_order_tables_id_to_autogenerated.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `go2nurse_service_category` CHANGE COLUMN `id` `id` BIGINT(64) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE `go2nurse_service_item` CHANGE COLUMN `id` `id` BIGINT(64) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE `go2nurse_service_order` CHANGE COLUMN `id` `id` BIGINT(64) NOT NULL AUTO_INCREMENT ;
