c562880fbb643ae03bc2bbc60b27bf73052f3d81
lisongzhao0 <lisongzhao0@msn.cn>
2016-08-02 20:05:46 +0800

    add PlatformType go2nurse_ios go2nurse_android go2nurse_backend_service

--- common/src/main/java/com/cooltoo/constants/PlatformType.java
+++ common/src/main/java/com/cooltoo/constants/PlatformType.java
@@ -4,10 +4,7 @@ package com.cooltoo.constants;
  * Created by yzzhao on 5/22/16.
  */
 public enum PlatformType {
-    NURSEGO_IOS,
-    NURSEGO_ANDROID,
-    NURSEGO_BACKEND_SERVICE,
-    BACKEN_ADMIN_WEB
+    IOS,ANDROID,BACKEND_SERVICE,BACKEN_ADMIN_WEB
     ,GO2NURSE_IOS
     ,GO2NURSE_ANDROID
     ,GO2NURSE_BACKEND_SERVICE
