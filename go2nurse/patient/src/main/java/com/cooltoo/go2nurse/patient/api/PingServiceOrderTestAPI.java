8a9ad386486b1ac6d163da82d550bf98b3b2ed7b
ZhaoYi <yzzhao@thoughtworks.com>
2016-08-04 20:34:00 +0800

    add pingpp test api

--- go2nurse/patient/src/main/java/com/cooltoo/go2nurse/patient/api/PingServiceOrderTestAPI.java
+++ go2nurse/patient/src/main/java/com/cooltoo/go2nurse/patient/api/PingServiceOrderTestAPI.java
@@ -1,16 +1,23 @@
 package com.cooltoo.go2nurse.patient.api;
 
 import com.cooltoo.go2nurse.service.PingPPService;
+import com.cooltoo.go2nurse.service.ServiceOrderService;
+import com.google.common.io.CharStreams;
 import com.pingplusplus.model.Charge;
+import com.pingplusplus.model.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 /**
  * Created by yzzhao on 7/30/16.
@@ -23,24 +30,42 @@ public class PingServiceOrderTestAPI {
     @Autowired
     private PingPPService pingPPService;
 
+    @Autowired
+    private ServiceOrderService orderService;
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createOrder(@Context HttpServletRequest request, @FormParam("channel") @DefaultValue("wx") String channel){
-        logger.info("get channel "+channel);
+    public Response createOrder(@Context HttpServletRequest request, @FormParam("channel") @DefaultValue("wx") String channel) {
+        logger.info("get channel " + channel);
         int amount = 100;
-        String orderNo="xxxx";
-        String ip="127.0.0.1";
-        String subject="test order";
-        String body="order description";
-        String description="order extra descritpion";
+        String orderNo = "xxxx";
+        String ip = request.getRemoteHost();
+        String subject = "test order";
+        String body = "order description";
+        String description = "order extra descritpion";
         Charge charge = pingPPService.createCharge(orderNo, channel, amount, ip, subject, body, description);
+
         return Response.ok(charge).build();
     }
 
     @POST
     @Path("/payment")
-    public Response payment(@Context HttpServletRequest request){
-        System.out.println("receive web hooks");
+    public Response payment(@Context HttpServletRequest request) {
+        logger.info("receive web hooks");
+        try {
+            ServletInputStream inputStream = request.getInputStream();
+            Reader reader = new InputStreamReader(inputStream);
+            String body = CharStreams.toString(reader);
+            logger.info("receive body "+body);
+            if(body != null) {
+                Event event = Event.GSON.fromJson(body, Event.class);
+                Charge charge = (Charge) event.getData().getObject();
+                orderService.orderChargeWebhooks(event.getId(), charge.getId(), body);
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
         return Response.ok().build();
     }
+
 }
