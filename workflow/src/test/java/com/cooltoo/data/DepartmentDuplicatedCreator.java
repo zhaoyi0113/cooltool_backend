ce7963a325d7f4d240174250597e56296ab8746e
lisongzhao0 <lisongzhao0@msn.cn>
2016-08-21 18:28:23 +0800

    add duplicated department

--- workflow/src/test/java/com/cooltoo/data/DepartmentDuplicatedCreator.java
+++ workflow/src/test/java/com/cooltoo/data/DepartmentDuplicatedCreator.java
@@ -9,6 +9,8 @@ import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.domain.Sort;
@@ -20,6 +22,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -37,6 +40,8 @@ import java.util.Map;
         TransactionDbUnitTestExecutionListener.class})
 public class DepartmentDuplicatedCreator {
 
+    private static final Logger logger = LoggerFactory.getLogger(DepartmentDuplicatedCreator.class);
+
     @Autowired private HospitalDepartmentRepository departmentRepository;
     @Autowired private HospitalRepository hospitalRepository;
 
@@ -49,26 +54,33 @@ public class DepartmentDuplicatedCreator {
         );
         List<HospitalDepartmentEntity> allDepartmentInTable = departmentRepository.findByHospitalId(0, sort);
         int newDepartmentId = 0;
+        int departmentCount=0;
         for (HospitalDepartmentEntity tmp : allDepartmentInTable) {
             if (tmp.getId()>newDepartmentId) {
                 newDepartmentId = tmp.getId();
             }
+            departmentCount++;
         }
+        newDepartmentId += 300;
 
+        int hospitalCount = 0;
         Iterable<HospitalEntity> allHospitalInTable = hospitalRepository.findAll();
-        for(HospitalEntity hospital : allHospitalInTable) {
-            long count = departmentRepository.countByHospitalId(hospital.getId());
-            if (count>0) {
-                continue;
-            }
+        List<HospitalDepartmentEntity> newDepartments = new ArrayList<>();
+        for(HospitalEntity tmpHospital : allHospitalInTable) {
+            hospitalCount ++;
             Map<Integer, Integer> oldId2NewId = new HashMap<>();
-            for (HospitalDepartmentEntity tmp : allDepartmentInTable) {
+            for (HospitalDepartmentEntity tmpDepartment : allDepartmentInTable) {
                 newDepartmentId++;
-                HospitalDepartmentEntity newOne = cloneDepartment(newDepartmentId, hospital.getId(), tmp, oldId2NewId.get(tmp.getParentId()));
-                newOne = departmentRepository.save(newOne);
-                oldId2NewId.put(tmp.getId(), newOne.getId());
+                HospitalDepartmentEntity newOne = cloneDepartment(newDepartmentId, tmpHospital.getId(), tmpDepartment, oldId2NewId.get(tmpDepartment.getParentId()));
+                oldId2NewId.put(tmpDepartment.getId(), newOne.getId());
+                newDepartments.add(newOne);
             }
         }
+        logger.info("new count is {}, expected is {}", newDepartments.size(), hospitalCount*departmentCount);
+
+        String insertSql = insertSQL(newDepartments);
+        boolean writeSuccess = writeTempFile(insertSql);
+        logger.info("save department_duplicated_sql file success={}", writeSuccess);
     }
 
     private HospitalDepartmentEntity cloneDepartment(int newDepartmentId, int hospitalId, HospitalDepartmentEntity entity, Integer newParentId) {
@@ -91,4 +103,43 @@ public class DepartmentDuplicatedCreator {
 
         return newEntity;
     }
+
+    private String insertSQL(List<HospitalDepartmentEntity> departments) {
+        StringBuilder insertSql = new StringBuilder();
+        insertSql.append("INSERT INTO `cooltoo_hospital_department`(`id`,`hospital_id`,`name`,`description`,`enable`,`image_id`,`disable_image_id`,`parent_id`,`unique_id`) VALUES").append("\r\n");
+        for (HospitalDepartmentEntity tmp : departments) {
+            insertSql.append("    (");
+            insertSql.append("'").append(tmp.getId()).append("',");
+            insertSql.append("'").append(tmp.getHospitalId()).append("',");
+            insertSql.append("'").append(tmp.getName()).append("',");
+            insertSql.append("'").append(null==tmp.getDescription() ? "" : tmp.getDescription()).append("',");
+            insertSql.append("'").append(tmp.getEnable()).append("',");
+            insertSql.append("'").append(tmp.getImageId()).append("',");
+            insertSql.append("'").append(tmp.getDisableImageId()).append("',");
+            insertSql.append("'").append(tmp.getParentId()).append("',");
+            insertSql.append("'").append(tmp.getUniqueId()).append("'),\r\n");
+        }
+        int index = insertSql.lastIndexOf(",");
+        insertSql.deleteCharAt(index);
+        insertSql.append(";");
+        return insertSql.toString();
+    }
+
+    private boolean writeTempFile(String content) {
+        try {
+            File file = File.createTempFile("department_duplicate_", ".sql");
+            File parent = file.getParentFile();
+            File dest = new File(parent, "department_dup.sql");
+            FileOutputStream out = new FileOutputStream(file);
+            out.write(content.getBytes("UTF-8"));
+            System.out.println(file.renameTo(dest));
+            logger.info("department_duplicate file is {}", file.getAbsolutePath());
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
 }
