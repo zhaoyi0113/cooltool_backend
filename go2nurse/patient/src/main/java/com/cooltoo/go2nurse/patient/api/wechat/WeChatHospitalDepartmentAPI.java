8a948cb200f6552840f224e683f99c9aef854272
Zhao Yi <zhaoyi0113@gmail.com>
2016-10-21 16:58:05 +1100

    fix get hos dep name bug

--- go2nurse/patient/src/main/java/com/cooltoo/go2nurse/patient/api/wechat/WeChatHospitalDepartmentAPI.java
+++ go2nurse/patient/src/main/java/com/cooltoo/go2nurse/patient/api/wechat/WeChatHospitalDepartmentAPI.java
@@ -1,7 +1,11 @@
 package com.cooltoo.go2nurse.patient.api.wechat;
 
+import com.cooltoo.beans.HospitalBean;
 import com.cooltoo.beans.HospitalDepartmentBean;
+import com.cooltoo.exception.BadRequestException;
+import com.cooltoo.exception.ErrorCode;
 import com.cooltoo.services.CommonDepartmentService;
+import com.cooltoo.services.CommonHospitalService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +16,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zhaoyi0113 on 20/10/2016.
@@ -25,16 +31,34 @@ public class WeChatHospitalDepartmentAPI {
     @Autowired
     private CommonDepartmentService departmentService;
 
+    @Autowired private CommonHospitalService hospitalService;
+
     @Path("/department/{uniqueId}")
     @Produces(MediaType.APPLICATION_JSON)
     @GET
-    public Response getDepartmentInfo(@PathParam("uniqueId") String uniqueId) {
-        List<HospitalDepartmentBean> beans = departmentService.getDepartmentByUniqueId(uniqueId, "");
+    public Response getHosDepartmentName(@PathParam("uniqueId") String uniqueId) {
+        if(uniqueId == null || uniqueId.length()<12){
+            logger.error("Invalid department unique id "+uniqueId);
+            throw new BadRequestException(ErrorCode.DATA_ERROR);
+        }
+        String depUniqueId = uniqueId.substring(6,12);
+        List<HospitalDepartmentBean> beans = departmentService.getDepartmentByUniqueId(depUniqueId, "");
         if (beans.isEmpty()) {
             logger.error("failed to find department by unique id "+uniqueId);
-            return Response.ok().build();
+            throw new BadRequestException(ErrorCode.DATA_ERROR);
         }
         logger.info("get department " + beans.get(0));
-        return Response.ok(beans.get(0)).build();
+        String hosUniqueId = uniqueId.substring(0,6);
+        List<HospitalBean> hospitalBeens = hospitalService.getHospitalByUniqueId(hosUniqueId);
+        if(hospitalBeens.isEmpty()){
+            logger.error("get hospital failed "+hosUniqueId);
+            throw new BadRequestException(ErrorCode.DATA_ERROR);
+        }
+        Map<String, String> data = new HashMap<>();
+        data.put("name", hospitalBeens.get(0).getName()+beans.get(0).getName());
+        return Response.ok(data).build();
     }
+
+
+
 }
