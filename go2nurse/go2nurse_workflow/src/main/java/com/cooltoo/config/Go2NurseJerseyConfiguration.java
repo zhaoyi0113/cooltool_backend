06a3633c20c17f2ae658c071d53fd07dbff45294
lisongzhao0 <lisongzhao0@msn.cn>
2016-10-28 18:41:39 +0800

    go2nure -- bug - forget add course order API to application configuration

--- go2nurse/go2nurse_workflow/src/main/java/com/cooltoo/config/Go2NurseJerseyConfiguration.java
+++ go2nurse/go2nurse_workflow/src/main/java/com/cooltoo/config/Go2NurseJerseyConfiguration.java
@@ -81,6 +81,7 @@ public class Go2NurseJerseyConfiguration extends ResourceConfig {
         register(WeChatPayAPI.class);
         register(DoctorOrderManageAPI.class);
         register(WeChatHospitalDepartmentAPI.class);
+        register(CategoryCourseOrderManageAPI.class);
         property(ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR, "true");
 
     }
