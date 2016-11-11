18f473b33377033338c9c339a6ee3489ffadefcc
Zhao Yi <zhaoyi0113@gmail.com>
2016-11-11 18:04:48 +1100

    response headers log

--- nurse360/common/src/main/java/com/cooltoo/nurse360/filters/Nurse360CORSResponseFilter.java
+++ nurse360/common/src/main/java/com/cooltoo/nurse360/filters/Nurse360CORSResponseFilter.java
@@ -1,5 +1,8 @@
 package com.cooltoo.nurse360.filters;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -13,6 +16,8 @@ import java.io.IOException;
 @Provider
 public class Nurse360CORSResponseFilter implements ContainerResponseFilter {
 
+    private static final Logger logger = LoggerFactory.getLogger(Nurse360CORSResponseFilter.class);
+
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
@@ -20,6 +25,7 @@ public class Nurse360CORSResponseFilter implements ContainerResponseFilter {
         addResponseHeaders(headers, "Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
         headers.add("Access-Control-Allow-Headers", "access_token");
         headers.add("Access-Control-Allow-Headers", "ACCESS_TOKEN");
+        logger.debug("add response headers "+headers);
     }
 
     private void addResponseHeaders(MultivaluedMap<String, Object> headers, String key, String value){
