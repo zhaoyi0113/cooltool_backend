d5ccb0bad3b9b4ad8f14287308a74e7624c96775
Zhao Yi <zhaoyi0113@gmail.com>
2016-11-11 23:23:25 +1100

    restore

--- nurse360/nurse360_workflow/src/main/java/com/cooltoo/config/Nurse360SecurityConfiguration.java
+++ nurse360/nurse360_workflow/src/main/java/com/cooltoo/config/Nurse360SecurityConfiguration.java
@@ -4,10 +4,13 @@ package com.cooltoo.config;
  * Created by zhaolisong on 2016/10/20.
  */
 
+import com.cooltoo.nurse360.filters.hospital.Nurse360HospitalManagementFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 /**
  *
@@ -16,20 +19,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class Nurse360SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-//    @Autowired private Nurse360HospitalManagementFilter filter;
+    @Autowired
+    private Nurse360HospitalManagementFilter filter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //
         // add custom filter for user authentication to all hospital module url
         //
-//        http.antMatcher("/nurse360/hospital_management/**")
-//            .addFilterBefore(filter, BasicAuthenticationFilter.class);
+        http.antMatcher("/nurse360/hospital_management/**")
+            .addFilterBefore(filter, BasicAuthenticationFilter.class);
 
         //
         // Cross-site request forgery filter disabled
         //
-//        http.csrf().disable();
+        http.csrf().disable();
 
     }
 }
