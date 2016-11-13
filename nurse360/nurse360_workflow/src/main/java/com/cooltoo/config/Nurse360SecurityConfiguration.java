01845aa43286e5822a1bacb3e191ed5accd5abd7
Zhao Yi <zhaoyi0113@gmail.com>
2016-11-13 12:35:01 +1100

    add ignore on security

--- nurse360/nurse360_workflow/src/main/java/com/cooltoo/config/Nurse360SecurityConfiguration.java
+++ nurse360/nurse360_workflow/src/main/java/com/cooltoo/config/Nurse360SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -35,11 +36,11 @@ public class Nurse360SecurityConfiguration extends WebSecurityConfigurerAdapter 
     @Autowired
     private ExceptionHandlerFilter exceptionHandlerFilter;
 
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        logger.info("configure wb security");
-//        web.ignoring().regexMatchers("!hospital_management");
-//    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        logger.info("configure wb security");
+        web.ignoring().regexMatchers("!hospital_management");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
