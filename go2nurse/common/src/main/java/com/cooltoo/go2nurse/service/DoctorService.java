08be4ea8367d9dc1fd448ace1e20ba000902839e
lisongzhao0 <lisongzhao0@msn.cn>
2016-11-22 21:05:25 +0800

    go2nurse -- bug - course replace with html is error when the image tag url could not fetched - modify 3rd times

--- go2nurse/common/src/main/java/com/cooltoo/go2nurse/service/DoctorService.java
+++ go2nurse/common/src/main/java/com/cooltoo/go2nurse/service/DoctorService.java
@@ -470,10 +470,10 @@ public class DoctorService {
             String httpPrefixUrl = "";
             // replace all temp http prefix url
             httpPrefixUrl = utility.getHttpPrefix()+tempStorage.getNginxRelativePath();
-            htmlContent.replace(httpPrefixUrl, "");
+            htmlContent = htmlContent.replace(httpPrefixUrl, "");
             // replace all user http prefix url
             httpPrefixUrl = utility.getHttpPrefix()+userStorage.getNginxRelativePath();
-            htmlContent.replace(httpPrefixUrl, "");
+            htmlContent = htmlContent.replace(httpPrefixUrl, "");
 
             //
             // move temporary file to official path
