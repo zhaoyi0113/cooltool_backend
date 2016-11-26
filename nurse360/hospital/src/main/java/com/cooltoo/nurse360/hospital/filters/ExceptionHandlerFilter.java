81aa45a955be64e63bbc591c73d807d05e9a4c97
joeyzhao0113 <joey@southbanksoftware.com>
2016-11-26 19:48:31 +1100

    skip options request on filter

--- nurse360/hospital/src/main/java/com/cooltoo/nurse360/hospital/filters/ExceptionHandlerFilter.java
+++ nurse360/hospital/src/main/java/com/cooltoo/nurse360/hospital/filters/ExceptionHandlerFilter.java
@@ -21,7 +21,6 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        logger.debug("exception handler filter");
         try {
             filterChain.doFilter(request, response);
         } catch (Exception e) {
