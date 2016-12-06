1f29738f51e1794eee9d89a1a715d8f137e418dd
lisongzhao0 <lisongzhao0@msn.cn>
2016-12-06 10:55:26 +0800

    go2nurse -- bug sql statment has database description(cooltoo)

--- dbschema/src/main/resources/db/migration/V2016_12_05_12_51__add_discount_cent_income_cent_need_visit_patient_record_approved_to_service_item_table.sql
+++ dbschema/src/main/resources/db/migration/V2016_12_05_12_51__add_discount_cent_income_cent_need_visit_patient_record_approved_to_service_item_table.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `cooltoo`.`go2nurse_service_item`
+ALTER TABLE `go2nurse_service_item`
   ADD COLUMN `service_discount_cent`     INT(11) NULL DEFAULT '0',
   ADD COLUMN `server_income_cent`        INT(11) NULL DEFAULT '0',
   ADD COLUMN `need_visit_patient_record` INT(11) NULL DEFAULT '0',
