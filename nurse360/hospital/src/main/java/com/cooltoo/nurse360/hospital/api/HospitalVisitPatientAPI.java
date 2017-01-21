b74d937df33c358b90ed34a4f2745df0e9254dbc
lisongzhao0 <lisongzhao0@msn.cn>
2017-01-21 10:39:19 +0800

    nurse360_hospital -- bug - visit patient for print pdf is error

--- nurse360/hospital/src/main/java/com/cooltoo/nurse360/hospital/api/HospitalVisitPatientAPI.java
+++ nurse360/hospital/src/main/java/com/cooltoo/nurse360/hospital/api/HospitalVisitPatientAPI.java
@@ -345,7 +345,7 @@ public class HospitalVisitPatientAPI {
                                                                @RequestParam(defaultValue = "0", name = "user_id") long userId,
                                                                @RequestParam(defaultValue = "0", name = "patient_id") long patientId,
                                                                @RequestParam(defaultValue = "0", name = "index") int pageIndex,
-                                                               @RequestParam(defaultValue = "0", name = "index") int sizePerPage
+                                                               @RequestParam(defaultValue = "10", name = "index") int sizePerPage
     ) {
         HospitalAdminUserDetails userDetails = SecurityUtil.newInstance().getUserDetails(SecurityContextHolder.getContext().getAuthentication());
         Long[] tmp = SecurityUtil.newInstance().getHospitalDepartmentLongId("", "", userDetails);
