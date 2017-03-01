e0a92a81395b662c6555180c98a6359a8c0d669c
lisongzhao0 <lisongzhao0@msn.cn>
2017-03-01 10:02:54 +0800

    go2nurse -- add setting score and suggestion of order API  2

--- dbschema/src/main/resources/db/migration/V2017_03_01_09_10__add_score_and_suggestion_to_service_order.sql
+++ dbschema/src/main/resources/db/migration/V2017_03_01_09_10__add_score_and_suggestion_to_service_order.sql
@@ -1,5 +1,4 @@
-ALTER TABLE `go2nurse_service_order`
-  CHANGE COLUMN `score` `score_attitude` FLOAT NULL DEFAULT '0' AFTER `item_manage_by`,
-  ADD COLUMN `score_standard` FLOAT NULL DEFAULT 0,
-  ADD COLUMN `score_in_time` FLOAT NULL DEFAULT 0,
-  ADD COLUMN `opinions_suggestions` VARCHAR(300) NULL DEFAULT '';
+ALTER TABLE `go2nurse_service_order` CHANGE COLUMN `score` `score_attitude` FLOAT NULL DEFAULT '0' AFTER `need_visit_patient_record`;
+ALTER TABLE `go2nurse_service_order` ADD COLUMN `score_standard`       FLOAT NULL DEFAULT 0;
+ALTER TABLE `go2nurse_service_order` ADD COLUMN `score_in_time`        FLOAT NULL DEFAULT 0;
+ALTER TABLE `go2nurse_service_order` ADD COLUMN `opinions_suggestions` VARCHAR(300) NULL DEFAULT '';
